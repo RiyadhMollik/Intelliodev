7867154a6c7472ba669e0a9a10a41fd1a2cccdd7
tabrid <mollik15-5096@diu.edu.bd>
2025-09-29 03:36:04 +0600

    update dashboard search

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,37 @@ import { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'Intelliodev - AI-Powered Full Stack Solutions',
   description: 'Intelliodev delivers cutting-edge web solutions, leveraging AI automation to streamline development and enhance efficiency.',
+  keywords: [
+    'AI-powered solutions',
+    'full stack development',
+    'web development',
+    'AI automation',
+    'Intelliodev',
+    'digital transformation',
+    'business growth',
+    'cloud services',
+    'data science',
+    'engineering solutions'
+  ],
+  authors: [
+    { name: 'Intelliodev Team', url: 'https://www.intelliodev.com' }
+  ],
+  openGraph: {
+    title: 'Intelliodev - AI-Powered Full Stack Solutions',
+    description: 'Discover Intelliodev: Your partner in AI-driven web solutions, transforming businesses with innovative technology.',
+    url: 'https://www.intelliodev.com',
+    siteName: 'Intelliodev',
+    images: [
+      {
+        url: 'https://www.intelliodev.com/og-image.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'Intelliodev Open Graph Image',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 }
 
 export default function RootLayout({
